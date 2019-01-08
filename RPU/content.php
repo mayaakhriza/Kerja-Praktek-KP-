a650be7575e8a1a3e1871c013ec54f91af6d48da
mayaakhriza <maya_akhriza@yahoo.com>
2019-01-08 13:06:13 +0700

    Sistem Informasi

---
+++ RPU/content.php
@@ -0,0 +1,143 @@
+<section id="featured">
+			<!-- start slider -->
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<!-- Slider -->
+						<div id="main-slider" class="flexslider">
+							<ul class="slides">
+								<li>
+									<img src="img/slides/sejarahtnbbs.jpg" alt="" />
+									<div class="flex-caption">
+										<h3>Sejarah Singkat</h3>
+									</div>
+								</li>
+								<li>
+									<img src="img/slides/RPU.jpg" alt="" />
+									<div class="flex-caption">
+										<h3>Visi dan Misi</h3>
+									</div>
+								</li>
+								<li>
+									<img src="img/slides/strukturorganisasi.jpg" alt="" />
+									<div class="flex-caption">
+										<h3>Struktur Organisasi</h3>
+									</div>
+								</li>
+							</ul>
+						</div>
+						<!-- end slider -->
+					</div>
+				</div>
+			</div>
+
+
+
+		</section>
+		<section class="callaction">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="big-cta">
+							<div class="cta-text">
+								<h2><span>Rhino Protection Unit (RPU)</span> <br> Yayasan Badak Indonesia <br> Taman Nasional Bukit Barisan Selatan</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="content">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="row">
+							<div class="col-lg-3">
+								<div class="box">
+									<div class="box-gray aligncenter">
+										<h4>Fully responsive</h4>
+										<div class="icon">
+											<i class="fa fa-desktop fa-3x"></i>
+										</div>
+										<p>
+											Voluptatem accusantium doloremque laudantium sprea totam rem aperiam.
+										</p>
+
+									</div>
+									<div class="box-bottom">
+										<a href="#">Lihat Detail</a>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3">
+								<div class="box">
+									<div class="box-gray aligncenter">
+										<h4>Modern Style</h4>
+										<div class="icon">
+											<i class="fa fa-pagelines fa-3x"></i>
+										</div>
+										<p>
+											Voluptatem accusantium doloremque laudantium sprea totam rem aperiam.
+										</p>
+
+									</div>
+									<div class="box-bottom">
+										<a href="#">Lihat Detail</a>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3">
+								<div class="box">
+									<div class="box-gray aligncenter">
+										<h4>Customizable</h4>
+										<div class="icon">
+											<i class="fa fa-edit fa-3x"></i>
+										</div>
+										<p>
+											Voluptatem accusantium doloremque laudantium sprea totam rem aperiam.
+										</p>
+
+									</div>
+									<div class="box-bottom">
+										<a href="#">Lihat Detail</a>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3">
+								<div class="box">
+									<div class="box-gray aligncenter">
+										<h4>Valid HTML5</h4>
+										<div class="icon">
+											<i class="fa fa-code fa-3x"></i>
+										</div>
+										<p>
+											Voluptatem accusantium doloremque laudantium sprea totam rem aperiam.
+										</p>
+
+									</div>
+									<div class="box-bottom">
+										<a href="#">Lihat Detail</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- divider -->
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="solidline">
+						</div>
+					</div>
+				</div>
+				<!-- end divider -->
+
+				<?php
+
+					require_once('berandacontent.php');
+
+				?>
+
+			</div>
+		</section>
+		
---
+++ auth_php/functions/db.php
@@ -0,0 +1,11 @@
+<?php
+
+$host     = 'localhost:3308';
+$user     = 'root';
+$password = '';
+$db       = 'si_rpu';
+
+
+$link = mysqli_connect($host, $user, $password, $db) or die(mysqli_error());
+
+?>
